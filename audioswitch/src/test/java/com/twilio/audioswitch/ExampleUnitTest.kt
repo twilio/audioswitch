8510acc47c64a27b9674b94365f1ed3832a271c6
Aaron Alaniz <aalaniz@twilio.com>
2020-04-23 16:27:42 -0500

    Add CI boilerplate

--- audioswitch/src/test/java/com/twilio/audioswitch/ExampleUnitTest.kt
+++ audioswitch/src/test/java/com/twilio/audioswitch/ExampleUnitTest.kt
@@ -1,8 +1,7 @@
 package com.twilio.audioswitch
 
+import junit.framework.TestCase.assertEquals
 import org.junit.Test
-
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
