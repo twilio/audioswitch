9ae7e0ff6f0acac4d1e37f4de1b418d751f7f615
John Qualls <jaltonqualls@twilio.com>
2020-05-27 11:45:31 -0600

    Add default bluetooth device name if null is returned from the Android BluetoothDevice class

--- audioswitch/src/main/java/com/twilio/audioswitch/android/BluetoothDeviceWrapperImpl.kt
+++ audioswitch/src/main/java/com/twilio/audioswitch/android/BluetoothDeviceWrapperImpl.kt
@@ -2,8 +2,10 @@ package com.twilio.audioswitch.android
 
 import android.bluetooth.BluetoothDevice
 
+internal const val GENERIC_DEVICE_NAME = "Bluetooth"
+
 internal class BluetoothDeviceWrapperImpl(
     private val device: BluetoothDevice,
-    override val name: String = device.name,
+    override val name: String = device.name ?: GENERIC_DEVICE_NAME,
     override val deviceClass: Int? = device.bluetoothClass?.deviceClass
 ) : BluetoothDeviceWrapper
