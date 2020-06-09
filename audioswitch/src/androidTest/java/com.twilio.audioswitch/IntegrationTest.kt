8ed348a6f5ab9a42108b21a09a2793cbec212f9b
John Qualls <jaltonqualls@twilio.com>
2020-06-09 11:10:02 -0600

    Add base integration test class

---
+++ audioswitch/src/androidTest/java/com.twilio.audioswitch/IntegrationTest.kt
@@ -0,0 +1,11 @@
+package com.twilio.audioswitch
+
+import androidx.test.annotation.UiThreadTest
+import org.junit.Before
+
+open class IntegrationTest {
+
+    @UiThreadTest
+    @Before
+    fun setUp() {}
+}
