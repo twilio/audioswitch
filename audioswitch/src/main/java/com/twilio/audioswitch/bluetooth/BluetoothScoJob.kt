ea26e8a9a0ce6d1af42251ec0509b0c86b7c1fd8
John Qualls <jaltonqualls@twilio.com>
2020-06-10 17:22:45 -0600

    Add timeout for BluetoothScoJob

--- audioswitch/src/main/java/com/twilio/audioswitch/bluetooth/BluetoothScoJob.kt
+++ audioswitch/src/main/java/com/twilio/audioswitch/bluetooth/BluetoothScoJob.kt
@@ -2,31 +2,49 @@ package com.twilio.audioswitch.bluetooth
 
 import android.os.Handler
 import android.os.Looper
+import android.os.SystemClock
 import com.twilio.audioswitch.android.LogWrapper
+import java.util.concurrent.TimeoutException
 
 private const val TAG = "BluetoothScoManager"
+private const val TIMEOUT = 5000L
 
 internal class BluetoothScoJob(private val logger: LogWrapper) {
 
-    private var activateBluetoothHandler: Handler = Handler(Looper.getMainLooper())
-    private var activateBluetoothRunnable: Runnable? = null
+    private var bluetoothScoHandler: Handler = Handler(Looper.getMainLooper())
+    private var bluetoothScoRunnable: BluetoothScoRunnable? = null
 
     fun executeBluetoothScoJob(scoAction: () -> Unit) {
-        activateBluetoothRunnable = BluetoothScoRunnable(scoAction)
-        activateBluetoothHandler.post(activateBluetoothRunnable)
-        logger.d(TAG, "Scheduled bluetooth sco job")
+        if (bluetoothScoRunnable == null) {
+            bluetoothScoRunnable = BluetoothScoRunnable(scoAction)
+            bluetoothScoHandler.post(bluetoothScoRunnable)
+            logger.d(TAG, "Scheduled bluetooth sco job")
+        }
     }
 
     fun cancelBluetoothScoJob() {
-        activateBluetoothHandler.removeCallbacks(activateBluetoothRunnable)
-        logger.d(TAG, "Canceled bluetooth sco job")
+        bluetoothScoRunnable?.let {
+            bluetoothScoHandler.removeCallbacks(it)
+            bluetoothScoRunnable = null
+            logger.d(TAG, "Canceled bluetooth sco job")
+        }
     }
 
     inner class BluetoothScoRunnable(private val scoAction: () -> Unit) : Runnable {
+
+        private val startTime = SystemClock.elapsedRealtime()
+        private var elapsedTime = 0L
+
         override fun run() {
-            logger.d(TAG, "Invoking bluetooth sco action")
-            scoAction.invoke()
-            activateBluetoothHandler.postDelayed(this, 500)
+            if (elapsedTime < TIMEOUT) {
+                logger.d(TAG, "Invoking bluetooth sco action")
+                scoAction.invoke()
+                bluetoothScoHandler.postDelayed(this, 500)
+                elapsedTime = SystemClock.elapsedRealtime() - startTime
+            } else {
+                logger.e(TAG, "Bluetooth sco job timed out", TimeoutException())
+                cancelBluetoothScoJob()
+            }
         }
     }
 }
