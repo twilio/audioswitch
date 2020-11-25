4638a264c626665775f32760a8377add6d6882ee
John Qualls <jaltonqualls@twilio.com>
2020-11-25 16:24:43 -0700

    Change parent exception type

--- audioswitch/src/main/java/com/twilio/audioswitch/AudioDeviceException.kt
+++ audioswitch/src/main/java/com/twilio/audioswitch/AudioDeviceException.kt
@@ -1,3 +1,3 @@
 package com.twilio.audioswitch
 
-class AudioDeviceException(override val message: String) : RuntimeException(message)
+class AudioDeviceException(override val message: String) : Exception(message)
