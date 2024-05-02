961725112067c1ddbe3a32e08ce59ad8aa8db397
ocarevs <ocarevs@twilio.com>
2024-05-02 15:54:12 +0300

    Add supresslint

--- audioswitch/src/main/java/com/twilio/audioswitch/android/BluetoothDeviceWrapperImpl.kt
+++ audioswitch/src/main/java/com/twilio/audioswitch/android/BluetoothDeviceWrapperImpl.kt
@@ -1,9 +1,11 @@
 package com.twilio.audioswitch.android
 
+import android.annotation.SuppressLint
 import android.bluetooth.BluetoothDevice
 
 internal const val DEFAULT_DEVICE_NAME = "Bluetooth"
 
+@SuppressLint("MissingPermission")
 internal data class BluetoothDeviceWrapperImpl(
     val device: BluetoothDevice,
     override val name: String = device.name ?: DEFAULT_DEVICE_NAME,
